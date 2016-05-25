4de53f2a5c63bf76075dd25a972d21824b335c36
Jack Della <jacko1394@gmail.com>
2016-05-25 14:54:51 +1000

    biDir implemented and working :)

--- src/mazeSolver/BiDirectionalBFSSolver.java
+++ src/mazeSolver/BiDirectionalBFSSolver.java
@@ -1,31 +1,96 @@
 package mazeSolver;
 
-import maze.Maze;
+import maze.*;
+import java.util.*;
 
-/** 
- * Implements Bi-directional BFS maze solving algorithm.
- */
+//Implements Bi-directional BFS maze solving algorithm:
 public class BiDirectionalBFSSolver implements MazeSolver {
+
+	Queue<Cell> leftQueue = new LinkedList<>();
+	Queue<Cell> rightQueue = new LinkedList<>();
+	ArrayList<Cell> leftVisited = new ArrayList<>();
+	ArrayList<Cell> rightVisited = new ArrayList<>();
+	boolean found = false;
+	int cellCount = 1;
+	int[] sides;
 
 	@Override
 	public void solveMaze(Maze maze) {
-		// TODO Auto-generated method stub
+		if(maze.type == Maze.HEX) {  //Hex sides:
+			sides = new int[] {Maze.NORTHEAST, Maze.SOUTHEAST, Maze.NORTHWEST, Maze.SOUTHWEST, Maze.EAST, Maze.WEST};
+		} else {  //Normal sides:
+			sides = new int[] {Maze.NORTH, Maze.SOUTH, Maze.EAST, Maze.WEST};
+		}
 
-	} // end of solveMaze()
+		//Add end points to start of each queue:
+		leftQueue.add(maze.entrance);
+		rightQueue.add(maze.exit);
 
+		while(!found) {
+			//LEFT SIDE::::::::::::::::
+			//Checks if paths have met:
+			maze.drawFtPrt(leftQueue.peek());
+			if(!rightVisited.contains(leftQueue.peek())) { //NOT MET
+				ArrayList<Cell> n = getNeighbors(leftQueue.peek());
+
+				for(Cell c : n) {
+					if(!leftVisited.contains(c)) {
+						leftQueue.add(c);
+					}
+				}
+
+				leftVisited.add(leftQueue.remove());
+
+			} else {
+				found = true;
+				break;
+			}
+
+			//RIGHT SIDE::::::::::::::::
+			//Checks if paths have met:
+			maze.drawFtPrt(rightQueue.peek());
+			if(!leftVisited.contains(rightQueue.peek())) { //NOT MET
+				ArrayList<Cell> n = getNeighbors(rightQueue.peek());
+
+				for(Cell c : n) {
+					if(!rightVisited.contains(c)) {
+						rightQueue.add(c);
+					}
+				}
+
+				rightVisited.add(rightQueue.remove());
+
+			} else {
+				found = true;
+				break;
+			}
+			//Add 2 to cell count (left side + right side):
+			cellCount += 2;
+		}
+	}
+
+	//Returns an arraylist of neighboring cells that can be accessed:
+	private ArrayList<Cell> getNeighbors(Cell c) {
+		ArrayList<Cell> n = new ArrayList<>();
+		for(int i : sides) {
+			if(!c.wall[i].present) {
+				n.add(c.neigh[i]);
+			}
+		}
+
+		if(c.tunnelTo != null) {
+			n.add(c.tunnelTo);
+		}
+		//Return in random order:
+		Collections.shuffle(n);
+		return n;
+	}
 
 	@Override
-	public boolean isSolved() {
-		// TODO Auto-generated method stub
-		return false;
-	} // end of isSolved()
-
-
+	public boolean isSolved() {return found;}
 	@Override
 	public int cellsExplored() {
-		// TODO Auto-generated method stub
-		return 0;
-	} // end of cellsExplored()
+		return cellCount;
+	}
 
-} // end of class BiDirectionalBFSSolver
-
+}//END OF FILE
